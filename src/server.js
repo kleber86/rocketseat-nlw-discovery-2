fe9b2f202e049b4f20041bf3e49769ba3159d402
Kleber <klebernascimento@outlook.com.br>
2020-08-09 12:18:56 -0300

    finalizando o quarto dia

--- src/server.js
+++ src/server.js
@@ -46,6 +46,11 @@ const weekdays = [
    "Sábado",
 ]
 
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1 
+    return subjects[position]
+}
+
 function pageLanding(req, res) {
     return res.render("index.html")
 }
@@ -57,7 +62,16 @@ function pageStudy(req, res) {
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    console.log(data)
+   
+    const isNotEmpty = Object.keys(data).length > 0
+   
+    if (isNotEmpty){
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        return res.redirect("/study")
+    }
 }
 
 const express = require('express')
